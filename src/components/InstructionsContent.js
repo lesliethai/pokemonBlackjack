12dad8be2c3ca1ee0b285b4eb6bd2909ae593480
leslie <lesliethai@gmail.com>
2023-04-09 23:05:36 -0700

    changed background img

--- src/components/InstructionsContent.js
+++ src/components/InstructionsContent.js
@@ -9,8 +9,8 @@ const InstructionsContent = ({ displayInstructions, setDisplayInstructions }) =>
       <div className="dialogueBoxWrapper instructions">
         <h2>How to play:</h2>
         <ul>
-          <li>Winner will be the player who has the closest sum of cards to 21 without going over.</li>
-          <li><span className="controlSpan">Hit</span> - deals another card to active player</li>
+          <li>On game start, each player starts with two cards drawn. The goal of the game is to either reach a total sum of card values of 21 or have a score closest to 21, without going over.</li>
+          <li><span className="controlSpan">Hit</span> - active player draws another card and adds it to their current hand</li>
           <li><span className="controlSpan">Stand</span> - stops the card draw and moves on to another player or ends the game</li>
         </ul>
       </div>
